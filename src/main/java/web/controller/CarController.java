9ebfdf19c873fa8cdeec0870b9cc78909fd38fd9
Vladimir <yalt.vladimir@gmail.com>
2023-01-30 12:50:37 +0300

    Update CarController mapping 2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/")
 public class CarController {
     @Autowired
     CarService carService;
